2a473f53d0ca853f5901be125d7c88617dbe5b4e
周旭 <zhouxusoft@gmail.com>
2023-05-04 19:45:21 +0800

    ..

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask , redirect
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
 
 @app.route('/main')
 def main():
