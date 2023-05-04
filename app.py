f6dcc3c1c7e1282b86f93ca0cebc479f739ff901
周旭 <zhouxusoft@gmail.com>
2023-05-05 01:16:11 +0800

    基本登录注册页面

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/main')
 def main():
     return redirect('https://godxu.top')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    if data['name'] == 'godxu' and data['password'] == '123456':
+        return jsonify({'success': True})
+    else:
+        return jsonify({'success': False})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ const poncon = new Poncon()
 
 poncon.setPageList(['login', 'registration'])
 
-let currentpage = 'buyer'
+let currentkind = 'buyer'
 
 $('#loginbutton').click(function () {
     $(this).addClass('chooseactive')
@@ -12,6 +12,35 @@ $('#loginbutton').click(function () {
 $('#registrationbutton').click(function () {
     $(this).addClass('chooseactive')
     $('#loginbutton').removeClass('chooseactive')
+})
+
+$('#loginenterbutton').click(function () {
+    let loginname = $('#input-username-login').val()
+    let loginpassword = $('#input-password-login').val()
+    let kinds = $('input[name="option"]:checked').val()
+    if (kinds == 'option1') {
+        currentkind = 'buyer'
+    } else {
+        currentkind = 'vendor'
+    }
+    console.log(currentkind)
+    let toSend = {
+        name: loginname,
+        password: loginpassword,
+        kinds: currentkind
+    }
+    $.ajax({
+        url: '/login',
+        type: 'POST',
+        data: JSON.stringify(toSend),
+        contentType: 'application/json',
+        success: function(response) {
+            console.log(response)
+        },
+        error: function(error) {
+            console.log(error)
+        }
+    });
 })
 
 poncon.start()
